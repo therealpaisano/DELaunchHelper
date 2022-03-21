cab860a46cbe33b0b5eff103b7a762efb8ffb7c3
therealpaisano <99160676+therealpaisano@users.noreply.github.com>
2022-03-21 18:43:27 -0500

    User profile folder option and updated conditional statements

--- Launch Helper.ps1
+++ Launch Helper.ps1
@@ -9,10 +9,30 @@ $options = Get-Content ".\options.json" | Out-String | ConvertFrom-Json
 $new_ver = ""
 
 if (!$options.Steam_path) {
-    Write-Output "Select your Steam installation folder:"
+    Write-Output "Select your Steam installation folder in the following pop-up:"
+    Start-Sleep -Seconds 3
     $null = $FolderBrowser.ShowDialog()
     $options.Steam_path = $FolderBrowser.SelectedPath
 }
+
+if (!$options.version_switching) {
+    $options.version_switching = Read-Host -Prompt "Will you be swapping versions using this script? (y/n) (you can change this in options.json later)"
+}
+if ($options.version_switching -eq "y"){
+    if (!$options.profile_path) {
+        $ProfileFolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+        $ProfileFolderBrowser.Description = "Select your Steam profile directory (.../Steam/userdata/<id>/)"
+        $ProfileFolderBrowser.rootfolder = "MyComputer"
+        Write-Output "To switch versions dynamically, you must specify your steam profile folder (this is where DOOM's profile.bin is stored)"
+        Write-Output "Select your Steam profile directory in the following pop-up (.../Steam/userdata/<id>/)"
+        Start-Sleep -Seconds 6
+        $null = $ProfileFolderBrowser.ShowDialog()
+        $options.profile_path = $ProfileFolderBrowser.SelectedPath
+    }
+}
+
+
+
 
 
 if (!$options.default_version) {
@@ -37,55 +57,82 @@ if (!$options.default_version) {
 }
 
 
-if (!$options.RTSS_path) {
-    Write-Output "Select your RTSS.exe installation:"
-    $null = $FileBrowser.ShowDialog()
-    $options.RTSS_path = $FileBrowser.FileName
+if (!$options.RTSS) {
+    $RTSS = Read-Host -Prompt "Would you like to launch RTSS when running this script? (y/n) (you can change this in options.json later)"
+    $options.RTSS = $RTSS
 }
-
-if (!$options.Livesplit_path) {
-    Write-Output "Select your Livesplit.exe installation:"
-    $null = $FileBrowser.ShowDialog()
-    $options.Livesplit_path = $FileBrowser.FileName
-}
-
-
-$ahk = ""
-if (!$options.AHK) {
-    $ahk = Read-Host -Prompt "Do you use Autohotkey to change FPS? (y/n)"
-    if ($save -eq "y") {
-        $options.AHK = $ahk
+if ($options.RTSS -eq "y"){
+    if (!$options.RTSS_path) {
+        Write-Output "Select your RTSS.exe installation:"
+        Start-Sleep -Seconds 3
+        $null = $FileBrowser.ShowDialog()
+        $options.RTSS_path = $FileBrowser.FileName
     }
 }
 
 
-$old_ver = Get-Content ($options.Steam_path+"\steamapps\common\DOOMEternal\version.txt")
+if (!$options.Livesplit) {
+    $options.Livesplit = Read-Host -Prompt "Would you like to launch Livesplit when running this script? (y/n) (you can change this in options.json later)"
+}
+if ($options.Livesplit -eq "y"){
+    if (!$options.Livesplit_path) {
+        Write-Output "Select your Livesplit.exe installation:"
+        Start-Sleep -Seconds 3
+        $null = $FileBrowser.ShowDialog()
+        $options.Livesplit_path = $FileBrowser.FileName
+    }
+}
 
-Rename-Item -Path ($options.Steam_path+"\steamapps\common\DOOMEternal\") -NewName ("DOOMEternal" + $old_ver)
-Rename-Item -Path ($options.Steam_path+"\steamapps\common\DOOMEternal" + $new_ver + "\") -NewName "DOOMEternal"
 
-Rename-Item -Path ($options.Steam_path+"\userdata\126516426\782330\remote\PROFILE\profile.bin") -NewName ("profile" + $old_ver + ".bin")
-Rename-Item -Path ($options.Steam_path+"\userdata\126516426\782330\remote\PROFILE\profile" + $new_ver + ".bin") -NewName "profile.bin"
+if (!$options.AHK) {
+    $options.AHK = Read-Host -Prompt "Do you use Autohotkey to change FPS? (y/n) (you can change this in options.json later)"
+}
+
+if (!$options.Macro) {
+    $options.Macro = Read-Host -Prompt "Do you want to launch the Freescroll Emulation Macro with this script? (y/n) (you can change this in options.json later)"
+}
+
+if ($options.version_switching) {
+	$old_ver = Get-Content ($options.Steam_path+"\steamapps\common\DOOMEternal\version.txt")
+
+	Rename-Item -Path ($options.Steam_path+"\steamapps\common\DOOMEternal\") -NewName ("DOOMEternal" + $old_ver)
+	Rename-Item -Path ($options.Steam_path+"\steamapps\common\DOOMEternal" + $new_ver + "\") -NewName "DOOMEternal"
+
+	Rename-Item -Path ($options.profile_path+"\782330\remote\PROFILE\profile.bin") -NewName ("profile" + $old_ver + ".bin")
+	Rename-Item -Path ($options.profile_path+"\782330\remote\PROFILE\profile" + $new_ver + ".bin") -NewName "profile.bin"
+}
 
 Start-Process -FilePath ($options.Steam_path+"\steamapps\common\DOOMEternal\DOOMEternalx64vk.exe")
-Start-Process -FilePath $options.Livesplit_path
-Start-Process -FilePath $options.RTSS_path -Verb RunAs
-if ($ahk -eq "y") {
+if ($options.Livesplit -eq "y") {
+    Start-Process -FilePath $options.Livesplit_path
+}
+if ($options.RTSS -eq "y") {
+    Start-Process -FilePath $options.RTSS_path -Verb RunAs
+}
+if ($options.AHK -eq "y") {
     Start-Process -FilePath ".\FPS_Limiter.ahk" -Verb RunAs
 }
 
-Start-Sleep -Seconds 5
-Start-Process -FilePath ".\DOOMEternalMacro.exe"
+Start-Sleep -Seconds 8
+
+if ($options.Macro -eq "y") {
+    Start-Process -FilePath ".\DOOMEternalMacro.exe"
+}
 
 $options | ConvertTo-Json | Out-File ".\options.json"
 
 Read-Host -Prompt "Press Enter to exit (This will close DOOM Eternal and other apps launched from this script)"
 
 Stop-Process -Name "DOOMEternalx64vk"
-Stop-Process -Name "LiveSplit"
-Stop-Process -Name "DOOMEternalMacro" 
-if ($ahk -eq "y") {
+if ($options.Livesplit -eq "y") {
+    Stop-Process -Name "LiveSplit"
+}
+if ($options.Macro -eq "y") {
+    Stop-Process -Name "DOOMEternalMacro" 
+}
+if ($options.AHK -eq "y") {
     Stop-Process -Name "AutoHotkey"
 }
-
-Stop-Process -Name "RTSS"
+if ($options.RTSS -eq "y") {
+    Stop-Process -Name "RTSS"
+}
